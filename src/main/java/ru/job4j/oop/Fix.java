2dd41f96d9730389430e30ccda0cef16bba4589f
Алексей Алексей <Layko1982@mail.ru>
2021-03-21 22:32:31 +0300

    1.7. Конструктор [#444608]

---
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class Fix {
+
+    private String desc;
+
+    public Fix(String str) {
+        this.desc = str;
+    }
+
+    public static void main(String[] args) {
+        Fix item = new Fix("const");
+    }
+}
