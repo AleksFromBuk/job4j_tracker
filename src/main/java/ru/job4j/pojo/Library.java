a2c966ff8efea4fe055d010c349c6d19efd67186
AleksFromBuk <layko1982@mail.ru>
2021-04-04 12:45:20 +0300

    3. Массивы и модели. [#444622]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -1,15 +1,6 @@
 package ru.job4j.pojo;
 
 public class Library {
-    public static void swap(Book ob1, Book ob2) {
-        Book tmp = new Book(null, -1);
-        tmp.setName(ob1.getName());
-        tmp.setCount(ob1.getCount());
-        ob1.setName(ob2.getName());
-        ob1.setCount(ob2.getCount());
-        ob2.setName(tmp.getName());
-        ob2.setCount(tmp.getCount());
-    }
     public static void main(String[] args) {
         Book ob1 = new Book("Clean Code", 462);
         Book ob2 = new Book("NiklausVirt - Алгоритмы и структуры данных", 358);
@@ -24,7 +15,8 @@ public class Library {
         for (int i = 0; i < data.length; i++) {
             System.out.println(data[i].getName() + '\t' + data[i].getCount());
         }
-        swap(data[0], data[3]);
+        data[0] = ob4;
+        data[3] = ob1;
         System.out.println();
         for (int i = 0; i < data.length; i++) {
             System.out.println(data[i].getName() + '\t' + data[i].getCount());
