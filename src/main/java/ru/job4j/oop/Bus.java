0331c950206d79c6be2c3d7cb360581538ff8dfc
AleksFromBuk <layko1982@mail.ru>
2021-04-19 01:26:55 +0300

    3.2. Зачем нужно приведение типов [#444560]

---
+++ src/main/java/ru/job4j/oop/Bus.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class Bus implements Vehicle {
+
+    public void move() {
+        System.out.println("- метод объекта \"Bus\" ");
+    }
+
+    @Override
+    public void identyObj() {
+        System.out.println("Средняя скорость движения в несколько " +
+                "в раз меньше средней скорости поездов");
+    }
+}
---
+++ src/main/java/ru/job4j/oop/Train.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class Train implements  Vehicle{
+
+    @Override
+    public void move() {
+        System.out.println("- метод объекта \"Train\" ");
+    }
+
+    @Override
+    public void identyObj() {
+        System.out.println("Средняя скорость движения — 140 км/ч");
+    }
+}
