e72bddaa3f8bf393de042c5cb9c8abeaf99fbc42
AleksFromBuk <layko1982@mail.ru>
2021-06-01 14:39:05 +0300

    2.5. Реализация класса StartUI. Изменение заявки [#500746]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -7,6 +7,17 @@ public class Tracker {
     private int ids = 1;
     private int size = 0;
 
+    private int indexOf(int id) {
+        int rsl = -1;
+        for(int index = 0; index < size; index++) {
+            if(items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
     public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
@@ -14,16 +25,20 @@ public class Tracker {
     }
 
 
+//    public Item findById(int id) {
+//        Item rsl = null;
+//        for (int index = 0; index < size; index++) {
+//            Item item = items[index];
+//            if (item.getId() == id) {
+//                rsl = item;
+//                break;
+//            }
+//        }
+//        return rsl;
+//    }
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int index = indexOf(id);
+        return index != - 1 ? items[index] : null;
     }
 
 //    public Item[] findAll() {
@@ -51,5 +66,15 @@ public class Tracker {
         }
         rsl = Arrays.copyOf(rsl, newSize);
         return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        boolean result = index != -1;
+        if (result) {
+            item.setId(id);
+            items[index] = item;
+        }
+        return result;
     }
 }
