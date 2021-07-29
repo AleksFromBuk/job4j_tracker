fa93b94fe8f89dac1211ea91b540b8f87d91b068
AleksFromBuk <layko1982@mail.ru>
2021-07-29 21:22:15 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("qwerty@gmail.ru", "Abu ali ибн Сина");
+        map.put("qwerty@mail.ru", "Avicena");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
