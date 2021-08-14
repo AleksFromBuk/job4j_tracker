7d58282ee2995385e329d5a821314f6421297dc3
AleksFromBuk <layko1982@mail.ru>
2021-08-14 18:43:01 +0300

    2. Отсортировать департаменты [#34352]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -41,10 +41,18 @@ public class DepartmentsTest {
         List<String> expected = Arrays.asList("K1", "K1/SK1", "K1/SK1/SSK1", "K1/SK1/SSK2", "K1/SK2",
                 "K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2", "K2/SK2");
         Departments.sortAsc(input);
-//        for (String it : input) {
-//            System.out.println(it);
-//        }
         assertThat(expected, is(input));
+    }
+
+    @Test
+    public void descOrderSortWithFillGaps() {
+        List<String> input = Departments.fillGaps(Arrays.asList("K1/SK1", "K1/SK2", "K1/SK1/SSK1",
+                "K1/SK1/SSK2", "K2", "K2/SK1/SSK1", "K2/SK1/SSK2"));
+
+        List<String> expected = Arrays.asList("K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2", "K1",
+                "K1/SK1", "K1/SK1/SSK1", "K1/SK1/SSK2", "K1/SK2");
+        Departments.sortDesc(input);
+        assertThat(input, is(expected));
     }
 
 }
