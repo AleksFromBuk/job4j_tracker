9996c7a02938b3a4a34fa5b09b71cbfb0358d1b1
Alexey Layko <Layko1982@mail.ru>
2022-09-05 21:53:34 +0300

    2. Список адресов [#110062] 1

--- src/main/java/ru/job4j/stream/addresslist/Profiles.java
+++ src/main/java/ru/job4j/stream/addresslist/Profiles.java
@@ -2,7 +2,6 @@ package ru.job4j.stream.addresslist;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
