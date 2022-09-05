5be499c91733b0b4cf861d68234f07d8d585f112
Alexey Layko <Layko1982@mail.ru>
2022-09-05 21:52:18 +0300

    2. Список адресов [#110062] 1

--- src/main/java/ru/job4j/stream/addresslist/Profiles.java
+++ src/main/java/ru/job4j/stream/addresslist/Profiles.java
@@ -4,11 +4,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
-
-    // Создать класс Profiles, в котором определить метод,
-    // который позволит преобразовать список профилей в список адресов.
-    // Каркас представлен ниже:
-
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(task -> task.getAddress())
