068a63a3753d257645b4716344f237a48666304a
AleksFromBuk <layko1982@mail.ru>
2022-09-06 10:26:46 +0300

    2. Список адресов [#110062 #339984] up

--- src/main/java/ru/job4j/stream/addresslist/Profiles.java
+++ src/main/java/ru/job4j/stream/addresslist/Profiles.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map(task -> task.getAddress())
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
 }
