44aafc2acc8e767ded2befbac0af542242443215
Alexey Layko <Layko1982@mail.ru>
2022-09-16 10:37:25 +0300

    1. Метод .uf [#70623 #343631]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -1,19 +1,19 @@
 package ru.job4j.collection;
 
 import org.junit.Test;
+
 import java.util.Arrays;
 import java.util.List;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-
-import static org.junit.Assert.*;
 
 public class DepartmentsTest {
 
     @Test
     public void whenMissed() {
         List<String> input = Arrays.asList("k1/sk1");
-        List<String> expected = Arrays.asList("k1", "k1/sk1");
+        List<String> expected = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expected));
     }
@@ -21,7 +21,7 @@ public class DepartmentsTest {
     @Test
     public void whenMoreMissed() {
         List<String> input = Arrays.asList("k1/sk1/ssk1");
-        List<String> expected = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk1");
+        List<String> expected = List.of("k1", "k1/sk1", "k1/sk1/ssk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expected));
     }
@@ -29,7 +29,7 @@ public class DepartmentsTest {
     @Test
     public void whenNonChange() {
         List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expected = Arrays.asList("k1", "k1/sk1");
+        List<String> expected = List.of("k1", "k1/sk1");
         List<String> rsl = Departments.fillGaps(input);
         assertThat(rsl, is(expected));
     }
@@ -38,7 +38,7 @@ public class DepartmentsTest {
     public void naturalOrderSortWithFillGaps() {
         List<String> input = Departments.fillGaps(Arrays.asList("K2/SK1/SSK1", "K2/SK1/SSK2",
                 "K2/SK2", "K1/SK1/SSK1", "K1/SK1/SSK2", "K1/SK2"));
-        List<String> expected = Arrays.asList("K1", "K1/SK1", "K1/SK1/SSK1", "K1/SK1/SSK2",
+        List<String> expected = List.of("K1", "K1/SK1", "K1/SK1/SSK1", "K1/SK1/SSK2",
                 "K1/SK2", "K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2", "K2/SK2");
         Departments.sortAsc(input);
         assertThat(expected, is(input));
@@ -49,7 +49,7 @@ public class DepartmentsTest {
         List<String> input = Departments.fillGaps(Arrays.asList("K1/SK1", "K1/SK2", "K1/SK1/SSK1",
                 "K1/SK1/SSK2", "K2", "K2/SK1/SSK1", "K2/SK1/SSK2"));
 
-        List<String> expected = Arrays.asList("K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2", "K1",
+        List<String> expected = List.of("K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2", "K1",
                 "K1/SK1", "K1/SK1/SSK1", "K1/SK1/SSK2", "K1/SK2");
 
         Departments.sortDesc(input);
